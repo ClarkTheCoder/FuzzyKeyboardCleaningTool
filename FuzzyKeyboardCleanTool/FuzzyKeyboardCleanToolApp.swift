3d97adece31eed94af2bcdd7e3f558ce6b397093
Carson Clark <carsonclark2009@gmail.com>
2023-10-11 00:02:24 -0700

    Initial Commit

---
+++ FuzzyKeyboardCleanTool/FuzzyKeyboardCleanToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuzzyKeyboardCleanToolApp.swift
+//  FuzzyKeyboardCleanTool
+//
+//  Created by carson on 2023-10-11.
+//
+
+import SwiftUI
+
+@main
+struct FuzzyKeyboardCleanToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
